d231471f0b4788a81b1b45fa2a090b0fa698bf08
alex kimathi <keemalekie@gmail.com>
2022-06-06 15:59:56 +0300

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(somevalue){
   // Write your code here!
+  let ride
+  if(somevalue < 400){
+    ride ='This one is on me!';
+  }else if(somevalue > 2000 && somevalue <= 2500){
+    ride = 'I will gladly take your thirty bucks.'
+  }else if (somevalue > 2500){
+    ride= 'No can do.'
+  }
+  return ride
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+
+  let message;
+  if(city === "NYC"){
+    message = "Ok, sounds good."
+  }else{
+    message = 'No go.'
+  }
+  return message
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+
+  let message
+  switch (tip) {
+    case tip ="generous":
+    message ='Thank you so much.'
+      
+      break;
+      case tip = "not as generous":
+      message = 'Thank you.';
+      break;
+  
+    default:
+      message = 'Bye.'
+      break;
+  }
+  return message
 }
